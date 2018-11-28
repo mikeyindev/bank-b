5f7fb3c285ba1979045b6744e420cbd667c1b0f8
mikeyindev <mikeyindev@gmail.com>
2018-11-28 10:24:14 -0600

    Removed EnableEurekaDiscovery annotation

--- src/main/java/com/mike/bank/BankApplication.java
+++ src/main/java/com/mike/bank/BankApplication.java
@@ -2,10 +2,8 @@ package com.mike.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class BankApplication {
 
 	public static void main(String[] args) {
